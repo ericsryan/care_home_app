d34d179f8e286993776223c895acab72706012d6
ericsryan <eric@ericsryan.com>
2021-09-22 18:29:13 -0700

    Added ability to view and edit an account

--- care_home_app/accounts/urls.py
+++ care_home_app/accounts/urls.py
@@ -5,6 +5,9 @@ from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-    path('login/', views.sign_in, name='login')
+    path('login/', views.sign_in, name='login'),
+    path('logout/', views.sign_out, name='logout'),
+    path('view/', views.view_account, name='view'),
+    path('edit/', views.edit_account, name='edit'),
 ]
 urlpatterns += staticfiles_urlpatterns()
--- care_home_app/accounts/views.py
+++ care_home_app/accounts/views.py
@@ -1,12 +1,14 @@
 from django.contrib.auth import login, logout
-from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import (AuthenticationForm, PasswordChangeForm,
+                                       UserChangeForm)
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
 
 def sign_in(request):
-    """Process the user login request"""
+    """Process the user login request."""
     form = AuthenticationForm
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
@@ -35,3 +37,31 @@ def sign_in(request):
         'accounts/login.html',
         {'form': form}
     )
+
+
+def sign_out(request):
+    """Log the user out of the site."""
+    logout(request)
+    # messages.success(request, "You've been logged out. Come back soon!")
+    return HttpResponseRedirect(reverse('index'))
+
+
+@login_required
+def view_account(request):
+    """Display the details of the user's account."""
+    user = request.user
+    return render(request, 'accounts/view_account.html', {'user': user})
+
+
+@login_required
+def edit_account(request):
+    """Edit the details of a user's account."""
+    if request.method == 'POST':
+        form = UserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your account has been updated.")
+            return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserChangeForm(instance=request.user)
+    return render(request, 'accounts/edit_account.html', {'form': form})
